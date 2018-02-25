cc28bb31ac5b74a33f8790114362f1ff3f80b74c
kavicse <30829826+kavicse@users.noreply.github.com>
2018-02-25 09:22:22 +0530

    Create task42.c

---
+++ task42.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int table,range,i,ans=0;
+	printf("\nEnter the table");
+	scanf("%d",&table);
+	printf("\nEnter the range");
+	scanf("%d",&range);
+	for(i=1;i<=range;i++)
+	{
+		ans=ans+15;
+			printf("\n table is %d",ans);
+	}
+	
+}
