35f7998280243662a70465d5c843f2e5c0a56bbe
kavicse <30829826+kavicse@users.noreply.github.com>
2018-02-25 09:15:10 +0530

    Create task24.c

---
+++ task24.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	float a,b,c;
+	printf("\nEnter value of a");
+	scanf("%f",&a);
+	printf("\nEnter value of b");
+	scanf("%f",&b);
+	c=a*b;
+	printf("\nOutput is %.2f",c);
+	
+}
