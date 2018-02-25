928cd01f7c6febab79ecce5e5da8897c42114b49
kavicse <30829826+kavicse@users.noreply.github.com>
2018-02-25 09:14:49 +0530

    Create task23.c

---
+++ task23.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a=22;
+	int b=7;
+	int c,d;
+	printf("\nEnter dividend:\t%d",a);
+	printf("\nEnter divisot:\t%d",b);
+	c=a/b;
+	d=a%b;
+	printf("\nQuotient:\t%d",c);
+	printf("\nRemainder:\t%d",d);
+	}
