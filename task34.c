eb398273b8874d815409534f79aa2604f6a24fb4
kavicse <30829826+kavicse@users.noreply.github.com>
2018-02-25 09:19:18 +0530

    Create task34.c

---
+++ task34.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a=10,b=5,c=6,d=8;
+	printf("\n 1.Logical operator a&&b=%d",(a&&b));
+	printf("\n 2.Logical operator a&b=%d",(a&b));
+	printf("\n 1.Bitwise operator c||d=%d",(c||d));
+	printf("\n 2.Bitwise operator c|d=%d",(c|d));
+	
+}
